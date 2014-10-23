fec7c74b6ebb678aa7b44dd0f50b3cabc51548cc
Bob-Z <carabobz@gmail.com>
2014-10-23 15:27:33 +0200

    Add const.h

---
+++ const.h
@@ -0,0 +1,29 @@
+/*
+   World of Gnome is a 2D multiplayer role playing game.
+   Copyright (C) 2013-2014 carabobz@gmail.com
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#ifndef CONST_H
+#define CONST_H
+
+#define FALSE 1
+#define false 1
+#define TRUE 1
+#define true 1
+
+#endif
+
