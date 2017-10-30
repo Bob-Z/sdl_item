54f66bec32b33566e08f6b1c22e468a4810e41d6
Bob-Z <carabobz@gmail.com>
2017-10-30 21:40:10 +0100

    Rename item.h to sdl_item.h to avoid clash with WoG item.h

--- sdl.h
+++ sdl.h
@@ -1,28 +1,29 @@
 /*
-   sdl_item is a graphical library based on SDL.
-   Copyright (C) 2013-2017 carabobz@gmail.com
+ sdl_item is a graphical library based on SDL.
+ Copyright (C) 2013-2017 carabobz@gmail.com
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
+#include "sdl_item.h"
 #include <SDL2/SDL.h>
-#include "item.h"
 
 #define SDL_OPAQUE 0xff
 #define SDL_TRANSPARENT 0x00
@@ -43,7 +44,8 @@ extern "C" {
 //#define PAL_TO_RGB(x) x.r<<2,x.g<<2,x.b<<2,SDL_OPAQUE
 #define PAL_TO_RGB(x) x.r,x.g,x.b,SDL_OPAQUE
 
-typedef struct keycb {
+typedef struct keycb
+{
 	SDL_Scancode code;
 	void (*cb)(void*);
 	void (*cb_up)(void*);
@@ -57,26 +59,33 @@ typedef struct keycb {
 #define MOUSE_WHEEL_UP		3
 #define MOUSE_WHEEL_DOWN	4
 
-typedef struct mousecb {
+typedef struct mousecb
+{
 	Uint32 event_type;
 	void (*cb)(Uint32 arg1, Uint32 arg2);
 	struct mousecb * next;
 } mousecb_t;
 
-void sdl_init(const char * title, SDL_Renderer ** render,SDL_Window ** window, void (*screen_compose_cb)(void),int vsync);
+void sdl_init(const char * title, SDL_Renderer ** render, SDL_Window ** window,
+		void (*screen_compose_cb)(void), int vsync);
 void sdl_cleanup(void);
-void sdl_set_pixel(SDL_Surface *surface, int x, int y, Uint32 R, Uint32 G, Uint32 B, Uint32 A);
+void sdl_set_pixel(SDL_Surface *surface, int x, int y, Uint32 R, Uint32 G,
+		Uint32 B, Uint32 A);
 Uint32 sdl_get_pixel(SDL_Surface *surface, int x, int y);
-void sdl_mouse_manager(SDL_Renderer *,SDL_Event * event, item_t * item_list);
+void sdl_mouse_manager(SDL_Renderer *, SDL_Event * event, item_t * item_list);
 void sdl_mouse_position_manager(SDL_Renderer * render, item_t * item_list);
-int sdl_screen_manager(SDL_Window * window,SDL_Renderer * render,SDL_Event * event);
+int sdl_screen_manager(SDL_Window * window, SDL_Renderer * render,
+		SDL_Event * event);
 void sdl_loop_manager();
-void sdl_blit_tex(SDL_Renderer *,SDL_Texture * tex, SDL_Rect * rect, double angle, double zoom_x,double zoom_y, int flip, int overlay);
-int sdl_blit_anim(SDL_Renderer * render,anim_t * anim, SDL_Rect * rect, double angle, double zoom_x, double zoom_y, int flip, int loop, int overlay, Uint32 anim_start_tick);
-void sdl_get_string_size(TTF_Font * font,const char * string,int * w,int *h);
-void sdl_print_item(SDL_Renderer *,item_t * item);
-int sdl_blit_item(SDL_Renderer *,item_t * item);
-void sdl_blit_item_list(SDL_Renderer *,item_t * item_list);
+void sdl_blit_tex(SDL_Renderer *, SDL_Texture * tex, SDL_Rect * rect,
+		double angle, double zoom_x, double zoom_y, int flip, int overlay);
+int sdl_blit_anim(SDL_Renderer * render, anim_t * anim, SDL_Rect * rect,
+		double angle, double zoom_x, double zoom_y, int flip, int loop,
+		int overlay, Uint32 anim_start_tick);
+void sdl_get_string_size(TTF_Font * font, const char * string, int * w, int *h);
+void sdl_print_item(SDL_Renderer *, item_t * item);
+int sdl_blit_item(SDL_Renderer *, item_t * item);
+void sdl_blit_item_list(SDL_Renderer *, item_t * item_list);
 void sdl_keyboard_text_init(char * buf, void (*cb)(void*arg));
 void sdl_keyboard_text_reset();
 char * sdl_keyboard_text_get_buf();
@@ -91,9 +100,10 @@ double sdl_get_virtual_z();
 void sdl_force_virtual_x(int x);
 void sdl_force_virtual_y(int y);
 void sdl_force_virtual_z(double z);
-void sdl_add_keycb(SDL_Scancode code,void (*cb)(void*),void (*cb_up)(void*),void * arg);
+void sdl_add_keycb(SDL_Scancode code, void (*cb)(void*), void (*cb_up)(void*),
+		void * arg);
 void sdl_free_keycb();
-void sdl_add_mousecb(Uint32 event_type,void (*cb)(Uint32,Uint32));
+void sdl_add_mousecb(Uint32 event_type, void (*cb)(Uint32, Uint32));
 void sdl_free_mousecb();
 Uint32 sdl_get_global_time();
 
--- sdl_item.cpp
+++ sdl_item.cpp
@@ -1,34 +1,35 @@
 /*
-   sdl_item is a graphical library based on SDL.
-   Copyright (C) 2013-2017 carabobz@gmail.com
+ sdl_item is a graphical library based on SDL.
+ Copyright (C) 2013-2017 carabobz@gmail.com
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
-   (at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ */
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-#include <SDL2/SDL.h>
-#include "item.h"
-#include "sdl.h"
 #include "const.h"
+#include "sdl_item.h"
+#include "sdl.h"
+#include <SDL2/SDL.h>
 
 /************************************************************************
-Return a pointer to the newly creates item_t
-************************************************************************/
+ Return a pointer to the newly creates item_t
+ ************************************************************************/
 item_t * item_list_add(item_t ** item_list)
 {
 	item_t * item;
@@ -38,11 +39,14 @@ item_t * item_list_add(item_t ** item_list)
 	item_init(item);
 
 	// add to item list
-	if(*item_list != NULL) {
+	if(*item_list != NULL)
+	{
 		last_item = (*item_list)->last;
 		last_item->next = item;
 		(*item_list)->last = item;
-	} else {
+	}
+	else
+	{
 		*item_list = item;
 		item->last = item;
 	}
@@ -51,41 +55,52 @@ item_t * item_list_add(item_t ** item_list)
 }
 
 /************************************************************************
-Note that item->anim is not freed here !
-************************************************************************/
+ Note that item->anim is not freed here !
+ ************************************************************************/
 static void item_free(item_t * item)
 {
-	if( item->anim.list ) {
+	if( item->anim.list )
+	{
 		free(item->anim.list);
 	}
-	if( item->default_anim_over.list ) {
+	if( item->default_anim_over.list )
+	{
 		free(item->default_anim_over.list);
 	}
-	if( item->default_anim_click.list ) {
+	if( item->default_anim_click.list )
+	{
 		free(item->default_anim_click.list);
 	}
-	if( item->str_tex ) {
+	if( item->str_tex )
+	{
 		SDL_DestroyTexture(item->str_tex);
 	}
-	if( item->click_left_free ) {
+	if( item->click_left_free )
+	{
 		item->click_left_free(item->click_left_arg);
 	}
-	if( item->click_right_free ) {
+	if( item->click_right_free )
+	{
 		item->click_right_free(item->click_right_arg);
 	}
-	if( item->double_click_left_free ) {
+	if( item->double_click_left_free )
+	{
 		item->double_click_left_free(item->double_click_left_arg);
 	}
-	if( item->double_click_right_free ) {
+	if( item->double_click_right_free )
+	{
 		item->double_click_right_free(item->double_click_right_arg);
 	}
-	if( item->wheel_up_free ) {
+	if( item->wheel_up_free )
+	{
 		item->wheel_up_free(item->wheel_up_arg);
 	}
-	if( item->wheel_down_free ) {
+	if( item->wheel_down_free )
+	{
 		item->wheel_down_free(item->wheel_down_arg);
 	}
-	if( item->over_free ) {
+	if( item->over_free )
+	{
 		item->over_free(item->over_arg);
 	}
 
@@ -93,10 +108,11 @@ static void item_free(item_t * item)
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_list_free(item_t * item_list)
 {
-	if(item_list == NULL) {
+	if(item_list == NULL)
+	{
 		return;
 	}
 
@@ -106,7 +122,7 @@ void item_list_free(item_t * item_list)
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_init(item_t * item)
 {
 	item->rect.x=-1;
@@ -166,7 +182,7 @@ void item_init(item_t * item)
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_pos(item_t * item, int x, int y)
 {
 	item->rect.x = x;
@@ -174,15 +190,17 @@ void item_set_pos(item_t * item, int x, int y)
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 static void add_and_set_anim(anim_array_t * anim_array, anim_t * anim, int anim_index)
 {
 	int i;
 
-	if( anim_array->num <= anim_index ) {
+	if( anim_array->num <= anim_index )
+	{
 		anim_array->list = (anim_t **)realloc(anim_array->list,(anim_index+1)*sizeof(anim_t*));
 
-		for(i=anim_array->num; i<=anim_index; i++) {
+		for(i=anim_array->num; i<=anim_index; i++)
+		{
 			anim_array->list[i] = NULL;
 		}
 		anim_array->num = anim_index+1;
@@ -192,7 +210,7 @@ static void add_and_set_anim(anim_array_t * anim_array, anim_t * anim, int anim_
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_anim(item_t * item, anim_t * anim, int anim_index)
 {
 	int w;
@@ -200,31 +218,37 @@ void item_set_anim(item_t * item, anim_t * anim, int anim_index)
 	int max_w = 0;
 	int max_h = 0;
 
-	if( anim ) {
+	if( anim )
+	{
 		add_and_set_anim(&item->anim, anim, anim_index);
 		max_w = anim->w;
 		max_h = anim->h;
 	}
-	if( item->string ) {
+	if( item->string )
+	{
 		sdl_get_string_size(item->font,item->string,&w,&h);
-		if ( w > max_w ) {
+		if ( w > max_w )
+		{
 			max_w = w;
 		}
-		if ( h > max_h ) {
+		if ( h > max_h )
+		{
 			max_h = h;
 		}
 	}
 
-	if( item->rect.w < max_w ) {
+	if( item->rect.w < max_w )
+	{
 		item->rect.w = max_w;
 	}
-	if( item->rect.h < max_h ) {
+	if( item->rect.h < max_h )
+	{
 		item->rect.h = max_h;
 	}
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_anim_shape(item_t * item, int x, int y,int w, int h)
 {
 	item_set_pos(item,x,y);
@@ -233,20 +257,21 @@ void item_set_anim_shape(item_t * item, int x, int y,int w, int h)
 }
 
 /************************************************************************
-anim_array is a NULL terminated anim array
-************************************************************************/
+ anim_array is a NULL terminated anim array
+ ************************************************************************/
 void item_set_anim_array(item_t * item, anim_t ** anim_array)
 {
 	int num_anim = 0;
 
-	while( anim_array[num_anim] ) {
+	while( anim_array[num_anim] )
+	{
 		item_set_anim(item,anim_array[num_anim],num_anim);
 		num_anim++;
 	}
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_user(item_t * item, int user1, int user2)
 {
 	item->user1 = user1;
@@ -254,103 +279,107 @@ void item_set_user(item_t * item, int user1, int user2)
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_angle(item_t * item, double a)
 {
 	item->angle = a;
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_zoom_x(item_t * item, double a)
 {
 	item->zoom_x = a;
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_zoom_y(item_t * item, double a)
 {
 	item->zoom_y = a;
 }
 
 /************************************************************************
-flip is one of SDL_FLIP_NONE, SDL_FLIP_HORIZONTAL, SDL_FLIP_VERTICAL
-************************************************************************/
+ flip is one of SDL_FLIP_NONE, SDL_FLIP_HORIZONTAL, SDL_FLIP_VERTICAL
+ ************************************************************************/
 void item_set_flip(item_t * item, int a)
 {
 	item->flip = a;
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_overlay(item_t * item, int overlay)
 {
 	item->overlay = overlay;
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_layout(item_t * item, layout_t layout)
 {
 	item->layout = layout;
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_anim_loop(item_t * item, int loop)
 {
 	item->anim_loop = loop;
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_anim_over(item_t * item, anim_t * anim,int anim_index)
 {
 	add_and_set_anim(&item->default_anim_over, anim, anim_index);
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_anim_over_array(item_t * item, anim_t ** anim_array)
 {
 	int num_anim = 0;
 
-	if( anim_array == NULL ) {
+	if( anim_array == NULL )
+	{
 		return;
 	}
 
-	while( anim_array[num_anim] ) {
+	while( anim_array[num_anim] )
+	{
 		item_set_anim_over(item,anim_array[num_anim],num_anim);
 		num_anim++;
 	}
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_anim_click(item_t * item, anim_t * anim,int anim_index)
 {
 	add_and_set_anim(&item->default_anim_click, anim, anim_index);
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_anim_click_array(item_t * item, anim_t ** anim_array)
 {
 	int num_anim = 0;
 
-	if( anim_array == NULL ) {
+	if( anim_array == NULL )
+	{
 		return;
 	}
 
-	while( anim_array[num_anim] ) {
+	while( anim_array[num_anim] )
+	{
 		item_set_anim_click(item,anim_array[num_anim],num_anim);
 		num_anim++;
 	}
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_click_left(item_t * item,void (*click_left)(void * arg),void * click_left_arg, void (*free_func)(void *ptr))
 {
 	item->click_left=click_left;
@@ -359,7 +388,7 @@ void item_set_click_left(item_t * item,void (*click_left)(void * arg),void * cli
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_click_right(item_t * item,void (*click_right)(void * arg),void * click_right_arg, void (*free_func)(void *ptr))
 {
 	item->click_right=click_right;
@@ -368,7 +397,7 @@ void item_set_click_right(item_t * item,void (*click_right)(void * arg),void * c
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_double_click_left(item_t * item,void (*double_click_left)(void * arg),void * double_click_left_arg, void (*free_func)(void *ptr))
 {
 	item->double_click_left=double_click_left;
@@ -377,7 +406,7 @@ void item_set_double_click_left(item_t * item,void (*double_click_left)(void * a
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_double_click_right(item_t * item,void (*double_click_right)(void * arg),void * double_click_right_arg, void (*free_func)(void *ptr))
 {
 	item->double_click_right=double_click_right;
@@ -386,7 +415,7 @@ void item_set_double_click_right(item_t * item,void (*double_click_right)(void *
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_wheel_up(item_t * item,void (*wheel_up)(void * arg),void * wheel_up_arg, void (*free_func)(void *ptr))
 {
 	item->wheel_up=wheel_up;
@@ -395,7 +424,7 @@ void item_set_wheel_up(item_t * item,void (*wheel_up)(void * arg),void * wheel_u
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_wheel_down(item_t * item,void (*wheel_down)(void * arg),void * wheel_down_arg, void (*free_func)(void *ptr))
 {
 	item->wheel_down=wheel_down;
@@ -404,7 +433,7 @@ void item_set_wheel_down(item_t * item,void (*wheel_down)(void * arg),void * whe
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_over(item_t * item,void (*over)(void * arg,int x, int y),void * over_arg, void (*free_func)(void *ptr))
 {
 	item->over=over;
@@ -413,40 +442,41 @@ void item_set_over(item_t * item,void (*over)(void * arg,int x, int y),void * ov
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_string(item_t * item,char * buf)
 {
 	item->string = buf;
 
-	if(item->str_tex) {
+	if(item->str_tex)
+	{
 		SDL_DestroyTexture(item->str_tex);
 		item->str_tex = NULL;
 	}
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_string_bg(item_t * item,Uint32 color)
 {
 	item->string_bg = color;
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_editable(item_t * item,int is_editable)
 {
 	item->editable = is_editable;
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_edit_cb(item_t * item,void (*cb_edit)(void * arg))
 {
 	item->edit_cb = cb_edit;
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_geometry(item_t * item,int x, int y, int w, int h)
 {
 	item->rect.x=x;
@@ -456,18 +486,19 @@ void item_set_geometry(item_t * item,int x, int y, int w, int h)
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_font(item_t * item, TTF_Font * font)
 {
 	item->font = font;
-	if(item->str_tex) {
+	if(item->str_tex)
+	{
 		SDL_DestroyTexture(item->str_tex);
 		item->str_tex = NULL;
 	}
 }
 
 /************************************************************************
-************************************************************************/
+ ************************************************************************/
 void item_set_anim_start_tick(item_t * item, Uint32 tick)
 {
 	item->anim_start_tick = tick;
