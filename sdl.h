d615d340604ddd76c51167a41851ec2e952ace69
Bob-Z <carabobz@gmail.com>
2020-01-06 14:36:27 +0100

    update copyright

--- sdl.h
+++ sdl.h
@@ -1,6 +1,6 @@
 /*
  sdl_item is a graphical library based on SDL.
- Copyright (C) 2013-2019 carabobz@gmail.com
+ Copyright (C) 2013-2020 carabobz@gmail.com
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
